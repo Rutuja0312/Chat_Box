a75d88360a658b39543318882a28640d263957b5
Rutuja0312 <114102397+Rutuja0312@users.noreply.github.com>
2022-09-24 12:54:51 +0530

    Add files via upload

---
+++ Chat_Box/src/main/java/com/chatbox/spring/ws/api/ChatBoxApplication.java
@@ -0,0 +1,13 @@
+package com.chatbox.spring.ws.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBoxApplication.class, args);
+	}
+}
+ 
---
+++ Chat_Box/src/test/java/com/chatbox/spring/ws/api/ChatBoxApplicationTests.java
@@ -0,0 +1,18 @@
+package com.chatbox.spring.ws.api;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChatBoxApplicationTests 
+{
+	@Test
+	void contextLoads() 
+	{
+		
+	}
+
+}
